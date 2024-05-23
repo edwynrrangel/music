c051ec25587e48096fc29aec868375f21f9b77a7
Edwyn Rangel <edwynrangel@gmail.com>
2024-05-23 00:01:11 -0500

    Remove unnecessary log statement in mongodb.go

--- go/multimedia/pkg/mongodb/mongodb.go
+++ go/multimedia/pkg/mongodb/mongodb.go
@@ -6,7 +6,6 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"fmt"
-	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -79,7 +78,6 @@ func (b *builder) setOptionAuth() *builder {
 }
 
 func (b *builder) getTLSConfig() (*tls.Config, error) {
-	log.Println("ca", b.ca)
 	caCert, err := base64.StdEncoding.DecodeString(b.ca)
 	if err != nil {
 		return nil, err
