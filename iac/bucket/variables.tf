3580e6474d680409eb93d0d6293fa01451b87abf
Edwyn Rangel <edwynrangel@gmail.com>
2024-05-28 21:38:11 -0500

    chore: Update bucket module to include multimedia readonly user secret

--- iac/bucket/variables.tf
+++ iac/bucket/variables.tf
@@ -1,12 +1,23 @@
 variable "minio_server" {
   description = "The MinIO server endpoint"
-  default     = "10.99.177.160:9000"
+  type        = string
+  sensitive   = true
 }
 
 variable "minio_user" {
   description = "The user for MinIO"
+  type        = string
+  sensitive   = true
 }
 
 variable "minio_password" {
   description = "The password for MinIO"
+  type        = string
+  sensitive   = true
+}
+
+variable "multimedia_readonly_user_secret" {
+    description = "The secret of the app readonly user"
+    type        = string
+    sensitive   = true
 }
