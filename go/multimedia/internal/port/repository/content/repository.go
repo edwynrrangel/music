07ffae97e7daabb0b9f4cbbda5230e1ae4e8dbf9
Edwyn Rangel <edwynrangel@gmail.com>
2024-05-30 09:32:35 -0500

    Implements the repositories that interact with the database

---
+++ go/multimedia/internal/port/repository/content/repository.go
@@ -0,0 +1,59 @@
+package content
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/edwynrrangel/grpc/go/multimedia/config"
+	"github.com/edwynrrangel/grpc/go/multimedia/internal/domain/content"
+)
+
+type repository struct {
+	dbClient       *mongo.Client
+	dbName         string
+	collectionName string
+}
+
+func NewRepository(dbClient *mongo.Client, config *config.Config) content.Repository {
+	return &repository{
+		dbClient:       dbClient,
+		dbName:         config.MongoDB.DbName,
+		collectionName: config.MongoDB.ContentCollectionnName,
+	}
+}
+
+func (r *repository) Get(ctx context.Context, id string) (*content.Content, error) {
+	objID, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.M{"_id": objID}
+
+	var content *content.Content
+	if err := r.dbClient.Database(r.dbName).Collection(r.collectionName).FindOne(ctx, filter).Decode(&content); err != nil {
+		return nil, err
+	}
+
+	return content, nil
+}
+
+func (r *repository) Search(ctx context.Context, query string) ([]content.Content, error) {
+	filter := bson.M{"$or": []bson.M{
+		{"title": bson.M{"$regex": query, "$options": "i"}},
+		{"genre": bson.M{"$regex": query, "$options": "i"}},
+		{"creator": bson.M{"$regex": query, "$options": "i"}},
+	}}
+
+	cursor, err := r.dbClient.Database(r.dbName).Collection(r.collectionName).Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var contents []content.Content
+	if err := cursor.All(ctx, &contents); err != nil {
+		return nil, err
+	}
+
+	return contents, nil
+}
