adc50c539dce458f57672c40b3ff204cefcc282f
Edwyn Rangel <edwynrangel@gmail.com>
2024-06-10 22:47:24 -0500

    feat: Add Kubernetes service configuration for multimedia server

---
+++ go/multimedia-server/iac/service.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_service" "multimedia_server_service" {
+  metadata {
+    name      = "${var.project_name}-service"
+    namespace = data.terraform_remote_state.infra.outputs.grpc_service_namespace
+    labels = var.labels
+  }
+  spec {
+    selector = var.selector_labels
+    type = "LoadBalancer"
+    port {
+      name     = "http"
+      protocol = "TCP" 
+      port     = var.app_port
+    }
+  }
+}
