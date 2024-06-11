72224d2121b21c2d6c8ed6bdacb36a53e0fab985
Edwyn Rangel <edwynrangel@gmail.com>
2024-06-10 19:25:00 -0500

    chore: Rename folder from multimedia to multimedia_server

--- go/multimedia_server/iac/outputs.tf
+++ go/multimedia_server/iac/outputs.tf
@@ -1,4 +1,3 @@
 output "grpc_server_url" {
   value = "${helm_release.grpc_multimedia_release.metadata[0].name}.${data.terraform_remote_state.infra.outputs.grpc_service_namespace}.svc.cluster.local:${var.app_port}"
-  
 }
