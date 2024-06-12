b10761b799e3a9fd58a970a8c70e4cd8e7a3fae2
Edwyn Rangel <edwynrangel@gmail.com>
2024-06-11 19:08:30 -0500

    refactor: Update variable name for content server URL output

--- go/content-server/iac/outputs.tf
+++ go/content-server/iac/outputs.tf
@@ -1,3 +1,3 @@
-output "grpc_server_url" {
+output "content_server_url" {
   value = "${kubernetes_service.content_server_service.metadata[0].name}.${data.terraform_remote_state.infra.outputs.grpc_service_namespace}.svc.cluster.local:${var.app_port}"
 }
