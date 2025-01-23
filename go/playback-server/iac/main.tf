0efc6669b5103f4ace79c9ddb5cb2399949309b1
Edwyn Rangel <edwynrangel@gmail.com>
2025-01-23 17:39:12 -0500

    Update Terraform state path for playback server in main.tf

--- go/playback-server/iac/main.tf
+++ go/playback-server/iac/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.12"
   backend "local" {
-    path = "/home/egui/terraform/music/apps/go/content-server/terraform.tfstate"
+    path = "/home/egui/terraform/music/apps/go/playback-server/terraform.tfstate"
   }
 }
 
